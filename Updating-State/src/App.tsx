6a2ca44cec8a6b234ed1d3111d7c084fc2ebb7fb
syth09 <tatuanphong616@gmail.com>
2024-09-14 21:24:34 +0700

    Ex3

--- Updating-State/src/App.tsx
+++ Updating-State/src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushrooms"]
+  const [cart, setCart] = useState({
+    discount: 1,
+    items: [
+      { id: 1, tittle: "Product 1", quantity: 1 },
+      { id: 2, tittle: "Product 2", quantity: 1 }
+    ]
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    });
   };
 
   return (
